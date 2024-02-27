ea2ad887ce8f021c7f8ce106f5e11b017145d325
raylu <90059+raylu@users.noreply.github.com>
2024-02-26 21:13:43 -0800

    get rid of sections

--- static/script.js
+++ static/script.js
@@ -3,25 +3,17 @@
 
 	const r = await fetch('files.json');
 	const files = await r.json();
+	const items = files.map((filename) => {
+		const [base, ext] = filename.split('.', 2);
+		const thumbnail = `${base}-thumb.${ext === 'mp4' ? 'avif' : ext}`;
+		return {'src': filename, 'srct': thumbnail};
+	});
 
-	for (const section of files) {
-		const el = document.createElement('section');
-		document.querySelector('body').append(el);
-		el.append(section['section']);
-
-		const div = document.createElement('div');
-		el.append(div);
-		const items = section['files'].map((filename) => {
-			const [base, ext] = filename.split('.', 2);
-			const thumbnail = `${base}-thumb.${ext === 'mp4' ? 'avif' : ext}`;
-			return {'src': filename, 'srct': thumbnail};
-		});
-		jQuery(div).nanogallery2({
-			'thumbnailHeight': 200,
-			'thumbnailWidth': 'auto',
-			'items': items,
-			'thumbnailBorderHorizontal': 0,
-			'thumbnailBorderVertical': 0,
-		});
-	}
+	jQuery('#nanogallery2').nanogallery2({
+		'thumbnailHeight': 200,
+		'thumbnailWidth': 'auto',
+		'items': items,
+		'thumbnailBorderHorizontal': 0,
+		'thumbnailBorderVertical': 0,
+	});
 })();

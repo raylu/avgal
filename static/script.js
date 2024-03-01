163176ce410b8034114d4d97146e98bedf8ebd2e
raylu <90059+raylu@users.noreply.github.com>
2024-02-29 20:27:31 -0800

    gallery by month

--- static/script.js
+++ static/script.js
@@ -3,13 +3,30 @@
 
 	const r = await fetch('files.json');
 	const files = await r.json();
-	const items = files.map((filename) => {
+	const items = [];
+	let lastMonth = null;
+	let albumID = 0;
+	for (const filename of files) {
 		const [base, ext] = filename.split('.', 2);
 		const thumbnail = `${base}-thumb.${ext === 'mp4' ? 'avif' : ext}`;
-		return {'src': filename, 'srct': thumbnail};
-	});
+
+		const month = filename.substring(0, 6);
+		if (month !== lastMonth) {
+			albumID++;
+			items.push({
+				'title': `${month.substring(0, 4)}-${month.substring(4)}`,
+				'ID': albumID, 'kind': 'album', 'src': thumbnail,
+			});
+			lastMonth = month;
+		}
+		items.push({'src': filename, 'srct': thumbnail, albumID});
+	}
 
 	jQuery('#nanogallery2').nanogallery2({
+		'thumbnailL1Height': 300,
+		'thumbnailL1Width': 'auto',
+		'thumbnailL1GutterWidth': 20,
+		'thumbnailL1GutterHeight': 20,
 		'thumbnailHeight': 200,
 		'thumbnailWidth': 'auto',
 		'items': items,
